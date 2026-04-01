044a3ed742ddef8be463f208bc9de6b9b3be8dfb
Utkarsh Srivastava <srivastava.utkarsh2118@gmail.com>
2026-04-01 14:01:20 +0530

    Delete frontend directory

--- frontend/assets/js/config.js
+++
@@ -1,7 +0,0 @@
-window.APP_CONFIG = {
-  API_BASE_URL: "http://127.0.0.1:5000/api",
-  PAYMENT_MODE: "qr",
-  DONATION_QR_IMAGE_URL: "",
-  DONATION_UPI_ID: "",
-  DONATION_UPI_NAME: "DonateSphere",
-};
